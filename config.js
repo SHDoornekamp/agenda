c6610e483a6f5d031275b030f33e97e6027af6c7
SHDoornekamp <s.h.doornekamp@campingnavigator.eu>
2026-09-18 14:52:52 +0200

    Update supabaseUrl in config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@
  * service_role key neer.
  */
 window.PLANNER_CONFIG = {
-  supabaseUrl: "https://bfpdjkkhsdwkvqkzeaqn.supabase.co/rest/v1/",
+  supabaseUrl: "https://bfpdjkkhsdwkvqkzeaqn.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmcGRqa2toc2R3a3Zxa3plYXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3Mjg1MjIsImV4cCI6MjEwNTMwNDUyMn0.frMuOaWOPuvWBWW2TfxjxDRpf9gj4_TbX86HgfSFl4k"
 };
